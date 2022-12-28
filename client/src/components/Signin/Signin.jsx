265534ec3fb07b718c1251c05448781f086f9a8b
Pradip Bedre <bedrepradip0@gmail.com>
2022-12-28 16:30:08 +0530

    signup and sign in pages created

--- client/src/components/Signin/Signin.jsx
+++ client/src/components/Signin/Signin.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
-const Signin = () => {
+import React from "react";
+import { FcGoogle } from "react-icons/Fc";
+import { Link } from "react-router-dom";
+const Signup = () => {
   return (
     <div>
-      Signin
+      <div className="signup-container">
+        <div className="left">
+          <img src="../../Assets/signup.png" alt="signup" />
+        </div>
+        <div className="right">
+          <h2>Register Now</h2>
+          <p>
+            or,
+            <Link to="/signup"> create a account</Link>
+          </p>
+          <form action="">
+            <label htmlFor="">Email:</label>
+            <input type="text" placeholder="email" />
+            <label htmlFor="">Mobile No:</label>
+            <input type="text" placeholder="password" />
+            <button className="signup">Sign In</button>
+            <button className="google">
+              <FcGoogle />
+              signup with google
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signin;
+export default Signup;
--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-
+import React from "react";
+import "./signup.scss";
+import { FcGoogle } from "react-icons/Fc";
+import { Link } from "react-router-dom";
 const Signup = () => {
   return (
     <div>
-      signup
+      <div className="signup-container">
+        <div className="left">
+          <img src="../../Assets/signup.png" alt="signup" />
+        </div>
+        <div className="right">
+          <h2>Register Now</h2>
+          <p>
+            or,
+            <Link to="/signin"> Login to account</Link>
+          </p>
+          <form action="">
+            <label htmlFor="">Email:</label>
+            <input type="text" placeholder="email" />
+            <label htmlFor="">Mobile No:</label>
+            <input type="text" placeholder="mobile no" />
+            <label htmlFor="">Password:</label>
+            <input type="text" placeholder="password" />
+            <label htmlFor="">Retype password:</label>
+            <input type="text" placeholder="retype password" />
+            <button className="signup">Sign Up</button>
+            <button className="google">
+              <FcGoogle />
+              signup with google
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Signup;
