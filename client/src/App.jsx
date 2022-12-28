67bbadf2ea5c4cc69ad2d47b0035ce87086e7f74
Pradip Bedre <bedrepradip0@gmail.com>
2022-12-28 13:39:42 +0530

    user dashboard compleated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Landingpage from "./pages/Landingpage";
+import Dashboard from "./pages/User/Dashboard";
+
 const App = () => {
   return (
     <div>
-      <Landingpage />
+      <Dashboard />
     </div>
   );
 };
