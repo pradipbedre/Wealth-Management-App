a56f5dea382eaf039cbf1f13b567623d407ec82d
Pradip Bedre <bedrepradip0@gmail.com>
2022-12-29 13:41:52 +0530

    profile page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,14 @@ import "./App.css";
 import LandingPage from "./pages/LandingPage/Landingpage";
 import Signup from "./components/Signup/Signup";
 import Signin from "./components/Signin/Signin";
+import Profile from "./components/UserProfile/Profile";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Profile />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
       </Routes>
---
+++ client/src/components/UserProfile/Profile.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./profile.scss";
+const Profile = () => {
+  return (
+    <div>
+      <div className="profile-container">
+        <div className="profile-navbar">
+          <h2>Your Profile</h2>
+          <div className="dropdown">
+            <img
+              className="dash-profile"
+              src="https://avatars.githubusercontent.com/u/60803643?s=40&v=4"
+              alt="profile-picture"
+            />
+            <div class="dropdown-content">
+              <button>Logout</button>
+            </div>
+          </div>
+        </div>
+        <div className="profile">
+          <div className="left">
+            <img
+              className="profile-img"
+              src="https://avatars.githubusercontent.com/u/60803643?s=40&v=4"
+              alt="profile-picture"
+            />
+            <div className="name-file">
+              <p>Pradip Bedre</p>
+              <input type="file" />
+            </div>
+          </div>
+          <div className="right">
+            <label htmlFor="">Name:</label>
+            <input type="text" placeholder="name" />
+            <label htmlFor="">Email:</label>
+            <input type="text" placeholder="email" />
+            <label htmlFor="">Password:</label>
+            <input type="text" placeholder="password" />
+            <label htmlFor="">Mobile No:</label>
+            <input type="text" placeholder="mobile no" />
+            <label htmlFor="">Address:</label>
+            <textarea rows="5" cols="60" name="text" placeholder="Address" />
+            <button>Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
