641121f13b7c81aa1805481825aba49acc4a6c80
Pradip Bedre <bedrepradip0@gmail.com>
2023-01-26 02:04:44 +0530

    all done

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api/": "http://localhost:3000/",
+      "/api/": "https://wealthup-backend.onrender.com/",
     },
   },
   plugins: [react()],
